721ed457ebe61b8fb6d693807c3e48c07ff261b5
Boniface Ebuka <boniface.ebuka@gmail.com>
2026-06-28 12:29:42 +0100

    feat: initialize React frontend project with Auth and Project scaffolding using React Query, MUI, and React Router

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -4,20 +4,33 @@ import NotFound from "../app/NotFound";
 import Login from "../features/auth/pages/Login";
 import Signup from "../features/auth/pages/Signup";
 import ProjectBoard from "../features/projects/pages/Projects";
+import CreateProject from "../features/projects/pages/CreateProjects";
+import AuthLayout from "../features/auth/layout/AuthLayout";
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CssBaseLine from '@mui/material/CssBaseline'
 
 const queryClient = new QueryClient();
 
 function App() {
+  const theme = createTheme({
+      palette: { mode: 'dark' }
+    })
   return (
-    <QueryClientProvider client={queryClient}>
+<QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
+      <CssBaseLine />
     <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path='/' element={<AuthLayout />}>
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<Signup />} />
+          </Route>
           <Route path="/projects" element={<ProjectBoard />} />
+          <Route path="/projects/create" element={<CreateProject />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+    </ThemeProvider>
   </QueryClientProvider>
   )
 }
--- frontend/src/features/auth/pages/Login.tsx
+++ frontend/src/features/auth/pages/Login.tsx
@@ -1,71 +1,114 @@
-import { useState } from "react";
+import { Box, Button, TextField } from '@mui/material'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import LoadingButton from '@mui/lab/LoadingButton'
+// import authApi from '../api/authApi'
 
-export default function Login() {
-  const [form, setForm] = useState({
-    email: "",
-    password: "",
-  });
+const Login = () => {
+  const navigate = useNavigate()
+  const [loading, setLoading] = useState(false)
+  const [usernameErrText, setUsernameErrText] = useState('')
+  const [passwordErrText, setPasswordErrText] = useState('')
 
-  const handleChange = (e) => {
-    setForm({
-      ...form,
-      [e.target.name]: e.target.value,
-    });
-  };
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setUsernameErrText('')
+    setPasswordErrText('')
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Login Data:", form);
+    const data = new FormData(e.target)
+    // const username = data.get('username').trim()
+    // const password = data.get('password').trim()
 
-    // Example API call
-    /*
-    fetch("/api/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(form),
-    });
-    */
-  };
+    // let err = false
+
+    // if (username === '') {
+    //   err = true
+    //   setUsernameErrText('Please fill this field')
+    // }
+    // if (password === '') {
+    //   err = true
+    //   setPasswordErrText('Please fill this field')
+    // }
+
+    // if (err) return
+
+    // setLoading(true)
+
+    // try {
+    //   const res = await authApi.login({ username, password })
+    //   setLoading(false)
+    //   localStorage.setItem('token', res.token);
+    //   navigate('/')
+    // } catch (err) {
+    //   const errors = err.data.errors
+    //   console.log(errors);
+    //   errors.forEach(e => {
+    //     if (e.param === 'username') {
+    //       setUsernameErrText(e.msg)
+    //     }
+    //     if (e.param === 'password') {
+    //       setPasswordErrText(e.msg)
+    //     }
+
+    //     if(e.path==='password'){
+    //       setPasswordErrText(e.msg);
+    //     }
+    //   })
+    //   setLoading(false)
+    // }
+  }
 
   return (
-    <div className="container">
-      <div className="auth-card">
-        <h2>Login</h2>
-
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label>Email</label>
-            <input
-              type="email"
-              name="email"
-              value={form.email}
-              onChange={handleChange}
-              placeholder="Enter your email"
-              required
-            />
-          </div>
-
-          <div className="form-group">
-            <label>Password</label>
-            <input
-              type="password"
-              name="password"
-              value={form.password}
-              onChange={handleChange}
-              placeholder="Enter your password"
-              required
-            />
-          </div>
-
-          <button type="submit">Login</button>
-        </form>
-
-        <p>
-          Don't have an account? <a href="/signup">Sign Up</a>
-        </p>
-      </div>
-    </div>
-  );
+    <>
+      <Box
+        component='form'
+        sx={{ mt: 1 }}
+        onSubmit={handleSubmit}
+        noValidate
+      >
+        <TextField
+          margin='normal'
+          required
+          fullWidth
+          id='username'
+          label='Username'
+          name='username'
+          disabled={loading}
+          error={usernameErrText !== ''}
+          helperText={usernameErrText}
+        />
+        <TextField
+          margin='normal'
+          required
+          fullWidth
+          id='password'
+          label='Password'
+          name='password'
+          type='password'
+          disabled={loading}
+          error={passwordErrText !== ''}
+          helperText={passwordErrText}
+        />
+        <LoadingButton
+          sx={{ mt: 3, mb: 2 }}
+          variant='outlined'
+          fullWidth
+          color='success'
+          type='submit'
+          loading={loading}
+        >
+          Login
+        </LoadingButton>
+      </Box>
+      <Button
+        component={Link}
+        to='/signup'
+        sx={{ textTransform: 'none' }}
+      >
+        Don't have an account? Signup
+      </Button>
+    </>
+  )
 }
+
+export default Login
